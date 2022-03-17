1100c45a09e7ca122d0315ba87c3b7a3e632a248
carlos-arman <minerocarlos28@gmail.com>
2022-03-16 18:32:31 -0700

    Programa_Java

---
+++ src/main/java/Desarrollador/crearMetodosConParametros.java
@@ -0,0 +1,28 @@
+
+package Ejercicios;
+
+
+public class crearMetodosConParametros {
+    public static void main(String[] args){
+        System.out.println("Metodo Void");
+        metodoConParametrosVoid(1, 24.34, "Lucas");
+        System.out.println("---------------");
+        System.out.println("Metodo int");
+        metodoConParametroInt(1);
+        System.out.println("---------------");
+        System.out.println("Metodo double");
+        metodoConParametroDoubles(24.34);
+    }
+
+    private static void metodoConParametrosVoid(int i, double d, String lucas) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void metodoConParametroInt(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void metodoConParametroDoubles(double d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
