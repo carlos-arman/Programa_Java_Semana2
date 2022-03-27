c648732dab744a04556c294e2cd31d6f61bdab6f
carlos-arman <minerocarlos28@gmail.com>
2022-03-27 11:47:23 -0700

    Avanse

--- src/main/java/Ejercicios/EJERCICIO5.java
+++ src/main/java/Ejercicios/EJERCICIO5.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Ejercicios;
 
-/**
- *
- * @author Armando
- */
+package Ejercicios;
+/*5-.  Escriba un programa para calcular el salario bruto de un empleado. 
+Para calcular el salario del empleado debemos de hacer los descuentos por 
+llegadas tardías así como la  retención de la renta que es el 10% del salario 
+menos el descuento por llegada tardía. Debe de crear una  fórmula  para  
+determinar  el  salario  por  minuto  y  calcular  el  monto  por  las  
+llegadas tardías y descontarlo  al  salario,  luego  debe  de  descontar  
+el  monto  de  la  renta.  El  programa  deberá  permitir  ingresar  el  
+nombre  delempleado, el salario mensual. El programa deberá de imprimir 
+el nombre del empleado, el descuento por llegadas tardías, la retención 
+de la renta y finalmente el salario bruto.*/
+
 public class EJERCICIO5 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
